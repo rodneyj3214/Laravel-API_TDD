c0b9c6dfa9d87ee4b5b15bc2deebb106c127a627
Rodney Jacome <rodneyj3214@gmail.com>
2021-04-17 21:49:30 -0500

    Agregar una vista para eliminar usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index(){
-
+        $users = User::latest()->get();
+        return view('users.index' ,['users' => $users]);
     }
-    public function store(){
-        
+    public function store(Request $request){
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password'=> $request->password,
+        ]);
+        return back();
     }
-    public function destroy(){
-        
+    public function destroy(User $user){
+        $user->delete();
+        return back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,4 @@ use App\Http\Controllers\UserController;
 
 Route::get('/', [UserController::class, 'index']);
 Route::post('users', [UserController::class, 'store']);
-Route::delete('users/{user}', [UserController::class, 'destroy']);
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
+Route::delete('users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
